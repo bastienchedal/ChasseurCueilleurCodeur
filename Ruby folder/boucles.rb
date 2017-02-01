0002c8b2359abdcaa5d8a58623e3084aa6fe319b
Alxmths <Alex@MacBook-Air-de-Alexandre.local>
2017-02-01 16:19:32 +0100

    mon premier commit, signé AM

---
+++ Ruby folder/boucles.rb
@@ -0,0 +1,16 @@
+utilisateurs = ["Alice", "Alexandre", "Bob"]
+
+utilisateurs.each do |nimportequoi|
+	nimportequoi= nimportequoi + "123"
+	puts nimportequoi
+end
+
+10.times do 
+	puts "Hello"
+end
+
+10.times do |m|
+	m=m+1
+	puts "Je sais compter jusqu'à #{m}"
+end
+
